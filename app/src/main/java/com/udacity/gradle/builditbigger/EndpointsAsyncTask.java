33318c773e2e209ff572119cd8bc45ff28287592
Prateek Sharma <sharma.prateek95@gmail.com>
2016-11-25 23:07:10 +0530

    added paid and free

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -2,12 +2,11 @@ package com.udacity.gradle.builditbigger;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.example.prateek.myapplication.backend.myApi.MyApi;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
 import java.io.IOException;
 
@@ -28,20 +27,26 @@ public class EndpointsAsyncTask extends AsyncTask <String, Void, String> {
     @Override
     protected String doInBackground(String... params){
      if(myApiService == null){
-         MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+
+
+         MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                 .setRootUrl("https://builditbigger-149110.appspot.com/_ah/api/");
+
+        /* MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                  new AndroidJsonFactory(), null)
-                 .setRootUrl("http://192.168.1.4:1234/_ah/api")
+                 .setRootUrl("http://10.0.2.2:1234/_ah/api")
                  .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                      @Override
                      public void initialize(AbstractGoogleClientRequest<?> request) throws IOException {
                          request.setDisableGZipContent(true);
                      }
-                 });
+                 });*/
          myApiService = builder.build();
      }
 
         try{
             isSuccess = true;
+            Log.v("LOG" ,"My api exected");
             return myApiService.displayJoke().execute().getJoke();
         }catch (IOException e){
             isSuccess = false;
--- backend/src/main/java/com/example/Prateek/myapplication/backend/MyEndpoint.java
+++ backend/src/main/java/com/example/Prateek/myapplication/backend/MyEndpoint.java
@@ -11,8 +11,6 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
-import javax.inject.Named;
-
 /**
  * An endpoint class we are exposing
  */
@@ -31,13 +29,13 @@ public class MyEndpoint {
      * A simple endpoint method that takes a name and says Hi back
      */
 
-    @ApiMethod(name = "sayHi")
+   /* @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
         response.setData("Hi, " + name);
 
         return response;
-    }
+    }*/
 
     @ApiMethod(name = "displayJoke")
     public MyJoke displayJoke(){
@@ -46,6 +44,7 @@ public class MyEndpoint {
 
         MyJoke resp = new MyJoke();
         resp.setJoke(seeJoke);
+
         return resp;
     }
 }
